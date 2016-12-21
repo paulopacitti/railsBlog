30f7e5cc42962b2932cbbe2bfba3aa268fa026e8
Paulo <ppacitti@outlook.com>
2016-12-21 10:55:29 -0200

    :ghost: - Migration to MySql and admin authentication

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,17 @@
 class CommentsController < ApplicationController
 
+	http_basic_authenticate_with name: "admin", password: "admin"
+		except [:index, :show]
+
 	def create
 		@article = Article.find(params[:article_id])
 		@comment = @article.comments.create(comment_params)
+		redirect_to article_path(@article)
+	end
+
+	def destroy
+		@article = Article.find(params[:article_id])
+		@comment = @article.comments.find(params[:id])
 		redirect_to article_path(@article)
 	end
 	
